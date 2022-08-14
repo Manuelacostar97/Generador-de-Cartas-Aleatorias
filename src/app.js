bc0fe81ae75fb7b14befc0935d954cc48619c446
Manuelacostar97 <manuelacostar97@gmail.com>
2022-08-14 19:50:33 +0000

    Finalizacion de proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = ["♦", "♥", "♠", "♣"];
+  let pintaIndex = Math.floor(Math.random() * pinta.length);
+  pinta = pinta[pintaIndex];
+  document.querySelector("#pintaTop").innerHTML = pinta;
+  document.querySelector("#pintaBot").innerHTML = pinta;
+
+  let color = ["red", "black"];
+  let colorIndex = Math.floor(Math.random() * color.length);
+  color = color[colorIndex];
+  document.querySelector("#pintaTop").style.color = color;
+  document.querySelector("#pintaBot").style.color = color;
+
+  document.querySelector("#cardNum").innerHTML = onloadCard();
+  function onloadCard() {
+    let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+    let numberIndex = Math.floor(Math.random() * number.length);
+    return number[numberIndex];
+  }
 };
