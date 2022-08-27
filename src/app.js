9fc3ab936583c1d529c07795f63b093c00bcccaa
Manuelacostar97 <manuelacostar97@gmail.com>
2022-08-27 03:27:00 +0000

    Correccion

--- src/app.js
+++ src/app.js
@@ -6,17 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let pintaTop = document.querySelector("#pintaTop");
+  let pintaBot = document.querySelector("#pintaBot");
+
   let pinta = ["♦", "♥", "♠", "♣"];
   let pintaIndex = Math.floor(Math.random() * pinta.length);
   pinta = pinta[pintaIndex];
-  document.querySelector("#pintaTop").innerHTML = pinta;
-  document.querySelector("#pintaBot").innerHTML = pinta;
 
-  let color = ["red", "black"];
-  let colorIndex = Math.floor(Math.random() * color.length);
-  color = color[colorIndex];
-  document.querySelector("#pintaTop").style.color = color;
-  document.querySelector("#pintaBot").style.color = color;
+  pintaTop.innerHTML = pinta;
+  pintaBot.innerHTML = pinta;
+
+  if (pinta === "♦" || pinta === "♥") {
+    pintaTop.style.color = "red";
+    pintaBot.style.color = "red";
+  }
+  if (pinta === "♠" || pinta === "♣") {
+    pintaTop.style.color = "black";
+    pintaBot.style.color = "black";
+  }
 
   document.querySelector("#cardNum").innerHTML = onloadCard();
   function onloadCard() {
@@ -25,4 +32,3 @@ window.onload = function() {
     return number[numberIndex];
   }
 };
-//test
